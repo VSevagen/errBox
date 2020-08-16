ffd2b6857df3fe58c0bc016220e6a064646416ed
sevagen <sevagenv@gmail.com>
2020-08-16 22:50:11 +0530

    admin: Add model to admin

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -1,14 +1,4 @@
 from django.contrib import admin
-from .models import Task, Username
+from .models import Songs
 
-# Register your models here.
-class MyModelAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Task
-
-class UsernameAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Username
-
-admin.site.register(Task, MyModelAdmin)
-admin.site.register(Username, UsernameAdmin)
+admin.site.register(Songs)
